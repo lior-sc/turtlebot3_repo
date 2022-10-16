6aa586e0a3652f5818a69fc5a2a47a3e805d0a32
lior-sc <lior@soltrex.io>
2022-10-17 00:02:13 +0300

    working on env

---
+++ shell/turtlebot3_PC_setup.sh
@@ -0,0 +1,18 @@
+
+###################### variables ######################
+turtlebot3_repo_path='/home/$USER/git/lior-sc/turtlebot3_repo'
+turtlebot3_ws_path='$turtlebot_repo_path/turtlebot3_ws'
+
+
+###################### functions ######################
+source_turtlebot3_env(){
+    source /opt/ros/foxy/setup.bash
+    source $turtlebot_ws_path/install/local_setup.bash
+    cd $turtlebot3_repo_path
+    
+    export ROS_DOMAIN_ID=30 
+    export TURTLEBOT_MODEL=burger
+    
+    echo "turtlebot3 env sourced"
+}
+
