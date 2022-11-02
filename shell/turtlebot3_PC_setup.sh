9a76c157ff847929ca9a1069243a4758701358bc
lior-sc <lior@soltrex.io>
2022-11-02 19:28:24 +0200

    lidar not displaying dots on RVIZ2

--- shell/turtlebot3_PC_setup.sh
+++ shell/turtlebot3_PC_setup.sh
@@ -1,13 +1,13 @@
 
 ###################### variables ######################
-turtlebot3_repo_path='/home/$USER/git/lior-sc/turtlebot3_repo'
-turtlebot3_ws_path='$turtlebot_repo_path/turtlebot3_ws'
+turtlebot3_repo_path="/home/$USER/git/lior-sc/turtlebot3_repo"
+turtlebot3_ws_path="$turtlebot3_repo_path/turtlebot3_ws"
 
 
 ###################### functions ######################
 source_turtlebot3_env(){
     source /opt/ros/foxy/setup.bash
-    source $turtlebot_ws_path/install/local_setup.bash
+    source $turtlebot3_ws_path/install/local_setup.bash
     cd $turtlebot3_repo_path
     
     export ROS_DOMAIN_ID=30 
