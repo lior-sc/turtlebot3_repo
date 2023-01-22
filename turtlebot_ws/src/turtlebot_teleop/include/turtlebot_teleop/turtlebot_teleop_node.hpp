2b39ab40133908a7f6875c47fabd71976a748a21
lior-sc <lior@soltrex.io>
2023-01-22 23:58:37 +0200

    working on joy subscriber. WIP

--- turtlebot_ws/src/turtlebot_teleop/include/turtlebot_teleop/turtlebot_teleop_node.hpp
+++ turtlebot_ws/src/turtlebot_teleop/include/turtlebot_teleop/turtlebot_teleop_node.hpp
@@ -1,6 +1,7 @@
 #ifndef TURTLEBOT_TELEOP_NODE_HPP
 #define TURTLEBOT_TELEOP_NODE_HPP
 
+#include <vector>
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/joy.hpp>
@@ -18,10 +19,18 @@ namespace liors_turtle
             bool init();
 
         private:
+            // variables
+            int joystick_lin_x_axis_;
+            int joystick_ang_z_axis_;
+            float turtlebot_max_lin_vel;
+            float turtlebot_max_ang_vel;
+
             // Members
 
             // Methods
-            void joy_sub_cb_(const sensor_msgs::msg::Joy::SharedPtr msg);
+            inline void declare_node_parameters();
+            inline void update_parameters_from_config();
+            bool convert_joy_to_vin(sensor_msgs::msg::Joy joy_msg, geometry_msgs::msg::Twist twist_value);
 
             // Subscribers
             rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
@@ -34,6 +43,9 @@ namespace liors_turtle
 
             // Timer callbacks
             void twist_pub_cb();
+
+            // Subscriber callbacks
+            void joy_sub_cb_(const sensor_msgs::msg::Joy::SharedPtr msg);
 
             /* data */
         };
--- turtlebot_ws/src/turtlebot_teleop/src/turtlebot_teleop_node.cpp
+++ turtlebot_ws/src/turtlebot_teleop/src/turtlebot_teleop_node.cpp
@@ -8,6 +8,7 @@ using namespace std::chrono_literals;
 TurtlebotTeleopNode::TurtlebotTeleopNode() : Node("turtlebot_teleop_node")
 {
     // declare variables
+    declare_node_parameters();
 
     // create publishers
     twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
@@ -17,9 +18,38 @@ TurtlebotTeleopNode::TurtlebotTeleopNode() : Node("turtlebot_teleop_node")
         "~/joy", 1, std::bind(&TurtlebotTeleopNode::joy_sub_cb_, this, std::placeholders::_1));
 }
 
+inline void TurtlebotTeleopNode::declare_node_parameters()
+{
+    this->declare_parameter<int>("joystick_lin_x_axis", 2);
+    this->declare_parameter<int>("joystick_ang_z_axis", 2);
+    this->declare_parameter<float>("turtlebot_max_lin_vel", 0.22);
+    this->declare_parameter<float>("turtlebot_max_ang_vel", 3.84);
+}
+
+inline void TurtlebotTeleopNode::update_parameters_from_config()
+{
+    this->get_parameter("joystick_lin_x_axis", joystick_lin_x_axis_);
+    this->get_parameter("joystick_ang_z_axis", joystick_ang_z_axis_);
+}
+
 bool TurtlebotTeleopNode::init()
 {
     // initialize timers
-    twist_pub_timer_ = this->create_wall_timer(20 * 1ms,
+    twist_pub_timer_ = this->create_wall_timer(20ms,
                                                std::bind(&TurtlebotTeleopNode::twist_pub_cb, this));
+    update_parameters_from_config();
+}
+
+bool TurtlebotTeleopNode::convert_joy_to_vin(sensor_msgs::msg::Joy joy_msg, geometry_msgs::msg::Twist twist_value)
+{
+    twist_value.linear.x = joy_msg.axes[joystick_lin_x_axis_] * turtlebot_max_lin_vel;
+    twist_value.angular.z = joy_msg.axes[joystick_ang_z_axis_] * turtlebot_max_ang_vel;
+}
+
+void TurtlebotTeleopNode::joy_sub_cb_(const sensor_msgs::msg::Joy::SharedPtr msg)
+{
+}
+
+void TurtlebotTeleopNode::twist_pub_cb()
+{
 }
